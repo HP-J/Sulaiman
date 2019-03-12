8c2e277e9991e6879ffb83207f2919e61210cd46
Kerolos Zaki <kerolos4zaki@gmail.com>
2019-03-12 05:29:06 +0200

    rewrote the index.js file, moved the main.js redirect to be hardcodded during electron-bulder's afterPack event, index.js always export api.js

--- afterPack.js
+++ afterPack.js
@@ -1,11 +1,24 @@
-var { copySync } = require('fs-extra');
+var { copySync, readJsonSync, writeJSONSync } = require('fs-extra');
 var { join } = require('path');
 
-exports.default = async function(context) {
-  var outputDir = join(context.appOutDir, '/resources/app/compiled');
-  var projectDir = join(context.packager.projectDir, '/compiled');
+exports.default = async function(context)
+{
+  var projectDir = context.packager.projectDir;
+  var outputDir = join(context.appOutDir, '/resources/app');
 
-  copySync(projectDir, outputDir);
+  // copy the compiled files as is
+  copySync(join(projectDir, '/compiled'), join(outputDir, '/compiled'));
   
   console.log('  • copied all compiled files into output directory successfully.');
+
+  // read package.json
+  const packageData = readJsonSync(join(projectDir, '/package.json'));
+
+  // override the main point to main.js
+  packageData.main = 'compiled/main/main.js';
+
+  // write the new package.json to the output directory
+  writeJSONSync(join(outputDir, '/package.json'), packageData);
+
+  console.log('  • updated package.json to point to main.js instead of index.js.');
 }
--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-// TODO rewrite a better npm package
-
-// const fs = require('fs');
-// const path = require('path');
-
-// if (!process.env.STARTED)
-// {
-//   require('./compiled/main/main.js');
-  
-//   process.env.STARTED = true;
-// }
-
-// if (fs.existsSync(path.join(__dirname, 'scr')))
-//   module.exports = require('./src/renderer/api.js');
+module.exports = require('./src/renderer/api.js');
